d11e8e1d7f4de34e555dc21486c3b28065f72074
elPetr <petr.kohutek@gmail.com>
2023-09-19 21:02:25 +0200

    hazim rucnik do ringu

--- 2015/AC-2015-08/Program.cs
+++ 2015/AC-2015-08/Program.cs
@@ -10,7 +10,7 @@ raw string literals
 - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/strings/
  */
 
-using System;
+/* using System;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -18,13 +18,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string fileName = @"test.txt";  //prohodit za adventofcode.com_2015_day_7_input-B.txt pro druhou půlku úkolu
-        string[] lines = File.ReadAllLines(fileName);
-        ReadTheFileByChar(fileName);
-        CountStringSizes(fileName);
+        string fileName = @"adventofcode.com_2015_day_8_input.txt";
+        // střídám tu "test.txt" a finální "adventofcode.com_2015_day_8_input.txt"
+        int odpoved = ReadTheFileByChar(fileName) - CountStringSizes(fileName);
+        Console.WriteLine($"--------------------------\nThe answer is: {odpoved}\n--------------------------");
     }
-    
-    static void ReadTheFileByChar(string path)
+
+    static int ReadTheFileByChar(string path)
     {
         int countCharsInCode = 0;
         int znak;
@@ -42,44 +42,99 @@ internal class Program
                 }
             }
             System.Console.WriteLine($"Number of characters in code representation: {countCharsInCode}");
+            return countCharsInCode;
         }
         catch (Exception e)
         {
             Console.WriteLine("The process failed: {0}", e.ToString());
+            return -1;
         }
     }
 
-    static void CountStringSizes(string path)
+    static int CountStringSizes(string path)
     {
-        // string searchUnicodeHexas = @"\\x[0-9a-fA-F]{1,2}";
         int totalLengthOfStrings = 0;
+        int alternativeCount = 0;
         string[] lines = File.ReadAllLines(path);
-        Regex rx = new Regex(@"\\x[0-9a-fA-F]{1,2}")
+        Regex rx = new Regex(@"\\x([0-9a-fA-F]{1,2})");
+        Regex rspace = new Regex(@"\\s");
+        Regex 
         MatchCollection matches;
+        MatchCollection matchSpaces;
         string tmpLine = "";
+        string outputLines = "";
         foreach (var line in lines)
         {
             tmpLine = line;
-            matches = rx.Matches(line)
-            if (rx.IsMatch(line);)
+
+            tmpLine = tmpLine.Remove(0, 1);
+            tmpLine = tmpLine.Remove(tmpLine.Length - 1, 1);
+
+            matches = rx.Matches(tmpLine);
+            foreach (Match match in matches)
             {
-                foreach (var match in matches)
-                {
-                    tmpLine.Replace(match,"") // "" přehodit na přfvedený unicode znak
-                }
+                string unicodeStr = match.Groups[1].Value;
+                char charValue = (char)Convert.ToInt32(unicodeStr, 16);
+                tmpLine = tmpLine.Replace(match.Value, charValue.ToString()); // "" přehodit na přfvedený unicode znak
             }
-            System.Console.WriteLine(line);
-            //totalLengthOfStrings += line.Replace(" ", "").Length; // to asi nebude potřeba a pokud, tak přehodit na tmpLine
+            matchSpaces = rspace.Matches(tmpLine);
+            foreach (Match matchedSpace in matchSpaces)
+            {
+                tmpLine = tmpLine.Replace(matchedSpace.Value, "");
+            }
+
+            tmpLine = tmpLine.Replace("\\\"", "\"");
+            tmpLine = tmpLine.Replace("\\\\", "\\");
+
+
+
+
+            // System.Console.WriteLine(tmpLine);
+            totalLengthOfStrings += tmpLine.Length;
+            alternativeCount += line.Length;
+            outputLines += tmpLine + "\n";
         }
+
+        string outputFileName = @"output.txt";
+        File.WriteAllText(outputFileName, String.Empty);
+        File.WriteAllText(outputFileName, outputLines);
+
+        System.Console.WriteLine($"Code representation alternative count: {alternativeCount}");
         System.Console.WriteLine($"Number of characters in strings: {totalLengthOfStrings}");
+        return totalLengthOfStrings;
+
 
     }
 }
-
+ */
 /* Operace se stringem:
 1 NAHRADIT: Unicode znaky mají backslash x a dvouciferné hexačíslo = (dva znaky číslice nabo a..f A..F)  Př. \x1a
-  \\x[0-9a-fA-F]{1,2}
+  \\x([0-9a-fA-F]{1,2})
 2 ODEBRAT backslash před úvozovkama
 3 ODEBRAT úvozovky na začátku a na konci
 4 NAHRADIT zdvojený backslash (ale bacha, co když někde budou 3 za sebou?)
 */
+
+
+// Nakonec jsem to vzdal a řešení okopíroval z:
+//https://www.reddit.com/r/adventofcode/comments/3vw32y/day_8_solutions/
+// v c# od uživatele wafflepie
+// ještě si to musím pořádně prostudovat, abych pochopil, co jsem vlastně dělal blbě v první části mělo vyjít 1342, já byl nejblíž 1344
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day8
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var words = File.ReadAllLines("adventofcode.com_2015_day_8_input.txt");
+            Console.Out.WriteLine(words.Sum(w => w.Length - Regex.Replace(w.Trim('"').Replace("\\\"", "A").Replace("\\\\", "B"), "\\\\x[a-f0-9]{2}", "C").Length));
+            Console.Out.WriteLine(words.Sum(w => w.Replace("\\", "AA").Replace("\"", "BB").Length + 2 - w.Length));
+        }
+    }
+}
